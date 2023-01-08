e16bdd2af4af45c2fa84bcb6612405acf95b34d7
mactkg <mactkg@gmail.com>
2023-01-08 11:50:02 +0900

    margin update

--- components/blogs/ArticleHeader.tsx
+++ components/blogs/ArticleHeader.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import { FC } from "react"
 import style from "./articleHeader.module.scss"
 
 const ArticleHeader: FC<{title: string, published_at: string}> = ({ title, published_at }) => {
-    return <header>
+    return <header className={style.header}>
         <h1 className={style.title}>{title}</h1>
         <h2>{published_at}</h2>
     </header>
