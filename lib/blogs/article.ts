61be1667842381af363f159d8168a9cb5175fc8f
mactkg <mactkg@gmail.com>
2023-01-15 18:43:02 +0900

    Syntax highlighting using shiki

--- lib/blogs/article.ts
+++ lib/blogs/article.ts
@@ -1,5 +1,6 @@
 import fm from "front-matter";
 import { marked } from "marked";
+import shiki from "shiki";
 import { pathToSlug } from "./files";
 
 export interface ArticleAttribute {
@@ -36,8 +37,44 @@ export class Article {
   async renderHTML(): Promise<string> {
     if (this.renderedHTML) return this.renderedHTML;
 
-    const html = await marked.parse(this.body, { async: true });
-    this.renderedHTML = html;
-    return html;
+    return new Promise((resolve) => {
+      // marked()の第三引数にcallbackが渡っていないと
+      // highlight optionがasyncにならないので、callback形式にする。
+      // remarkに移してもいいかも。
+      // https://github.com/markedjs/marked/blob/4aee878ac913e55941407897a8221040f8817b48/src/marked.js#L39-L106
+      marked(
+        this.body,
+        {
+          highlight: this.renderCode,
+        },
+        (_, result) => {
+          this.renderedHTML = result;
+          resolve(result);
+        }
+      );
+    });
+  }
+
+  // <pre>タグや<code>タグで囲わずにshikiでrenderした結果を返す
+  private renderCode(code: string, lang?: string, callback?: Function) {
+    const fn = async () => {
+      const highlighter = await shiki.getHighlighter({
+        theme: "github-light",
+      });
+      const tokens = highlighter.codeToThemedTokens(code, lang);
+      const html = shiki.renderToHtml(tokens, {
+        elements: {
+          pre({ children }) {
+            return children;
+          },
+          code({ children }) {
+            return children;
+          },
+        },
+      });
+
+      callback ? callback(undefined, html) : null;
+    };
+    fn();
   }
 }
